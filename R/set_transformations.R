99dac55fbdfa688c249210f6621ec5210b7d7b00
Serafin Schoch <serafin.schoch@gmx.ch>
2026-07-06 21:50:21 +0200

    refactor: add helper functions for dataset transformations

--- R/set_transformations.R
+++ R/set_transformations.R
@@ -1,21 +1,40 @@
-dataset_transfrom <- function(ds, to, x_axis = NULL, decompose_strategy = hirarchical_paths) {
+to_wide <- function(ds, x_axis = NULL) {
+  dataset_transfrom(ds, "wide", x_axis = x_axis)
+}
+to_long <- function(ds) {
+  dataset_transfrom(ds, "long", )
+}
+to_decomposed <- function(ds, strategy = hirarchical_paths, x_axis = NULL) {
+  dataset_transfrom(ds, "decomposed", x_axis, strategy)
+}
+
+dataset_transfrom <- function(
+  ds,
+  to,
+  x_axis = NULL,
+  decompose_strategy = hirarchical_paths
+) {
   from <- state(ds)
-  
+
   if (from == to) {
     return(ds)
   }
-  
+
   transition <- paste0(from, "_to_", to)
-  
-  result <- switch(transition,
+
+  result <- switch(
+    transition,
     "wide_to_long" = wide_to_long(ds),
     "wide_to_decomposed" = dataset_decompose(ds, strategy = decompose_strategy),
     "long_to_wide" = long_to_wide(ds, col = x_axis),
-    "long_to_decomposed" = dataset_decompose(long_to_wide(ds, col = x_axis), strategy = decompose_strategy),
+    "long_to_decomposed" = dataset_decompose(
+      long_to_wide(ds, col = x_axis),
+      strategy = decompose_strategy
+    ),
     "decomposed_to_wide" = dataset_compose(ds),
     "decomposed_to_long" = wide_to_long(dataset_compose(ds)),
     stop("Unsupported transformation from ", from, " to ", to)
   )
-  
+
   return(result)
 }
